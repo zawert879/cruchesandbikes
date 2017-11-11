12b13bfaac63a3867056264df10974be9c7972fa
Vitaly <v-shonov@mail.ru>
2017-11-11 17:42:46 +0300

    full created command /start

--- index.js
+++ index.js
@@ -1,97 +1,109 @@
-const TelegramBot = require('node-telegram-bot-api');
-
-const mysql = require('mysql');
-
-const cMysql = mysql.createPool({
-        database : 'test',
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        connectionLimit: 100
-    });
-
-cMysql.getConnection(function(err,conn){
-    if(err){
-        console.log("MYSQL: can't get connection from pool:",err)
-    }else {
-        conn.query("SELECT * FROM test",
-            function(er,rows){
-                if(er){
-                    conn.release();
-                    console.log("MYSQL: ERROR: ",err);
-                } else {
-                    conn.release();
-                    console.log(rows);
-                    // make someting with rows
-                }
-            });
-    }
-});
-
-
-
- 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '452480076:AAE4bSOiAeZO2iDmcdgnPfROZxVNUUEcu20';
- 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
- 
-// Matches "/echo [whatever]"
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
- 
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
- 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
- 
-// Listen for any kind of message. There are different kinds of
-// messages.
-
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-messageLog();
-addinsql(msg.text);
-// send a message to the chat acknowledging receipt of their message
-bot.sendMessage(chatId, 'Received your message');
-});
-
-
-
-
-
-
-function messageLog(msg){
-// console.log(msg);
-// console.log(msg.from.username+':'+msg.text);
-};
-
-function addinsql(name) {
-    cMysql.getConnection(function(err,conn){
-        if(err){
-            console.log("MYSQL: can't get connection from pool:",err)
-        }else {
-            conn.query("INSERT INTO test (name) VALUES ('"+name+"')",
-                function(er,rows){
-                    if(er){
-                        conn.release();
-                        console.log("MYSQL: ERROR: ",err);
-                    } else {
-
-                        conn.release();
-
-                        // make someting with rows
-                    }
-                });
-        }
-    });
-
-
+function formattedTime(){
+    var date = new Date();
+    var day =date.getDay();
+    var month =date.getMonth();
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = date.getSeconds();
+    var time = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds;
+    return time;
 }
 
+
+var accuweather = require('node-accuweather')()('MQ5mPDjyi50yNYJJmY0to8fGj8wnimyd');
+
+const token = '477996150:AAEj8lEiKLv4sZzMCIY-wufi29zg_O1IZb0';
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(token, {polling: true});
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    database : 'cruchesandbikes',
+    host     : '192.168.161.57',
+    user     : 'user',
+    password : 'JfLsbnRLOIh2kNYO',
+    connectionLimit: 100
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+let content;
+
+bot.onText(/\/start/, (msg) => {
+    content = true;
+    const chatId = msg.from.id;
+    bot.sendMessage(chatId, 'Добро пожаловать!');
+
+    var query = connection.query("SELECT * FROM users WHERE telegram_id = '"+chatId+"'", function (error, results, fields) {
+        console.log(results.length);
+        if (results.length == 0)  {
+            connection.query("INSERT INTO users (telegram_id) VALUES ('"+chatId+"')", function (error, results, fields) {
+                if (results['city'] == null ) {
+                    bot.sendMessage(chatId, 'Пожалуйста, введите ваш город(на английском):');
+                    bot.on('message', city => {
+                        if (content) {
+                            city = city.text;
+                            content = false;
+                            var sql = "UPDATE users SET city = '" + city + "' WHERE telegram_id = '" + chatId + "' ";
+
+                            connection.query(sql, (err, result) => {
+                                if (err) throw err;
+                                console.log("1 record inserted");
+                            });
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
+
+bot.onText(/\/погода/, (msg) => {
+    const chatId = msg.from.id;
+accuweather.getCurrentConditions("rostov-on-don", {unit: "Celsius"})
+    .then(function(result) {
+        bot.sendMessage(chatId, "В Ростове на дону "+result.Temperature+"  градусов по цельсию");
+    });
+
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+    bot.sendMessage(chatId, resp);
+});
+
+
+bot.onText(/\/напомни (.+) в (.+)/i, (msg, match) => {
+    var userId = msg.chat.id;
+    var text = match[1];
+    var time = match[2];
+    var date = formattedTime();
+    // notes.push( { 'uid':userId, 'time':time, 'text':text } );
+    var sql = "INSERT INTO reminders (user_id, text, date, created_at) VALUES ('"+userId+"', '"+text+"', '"+time+"', '"+date+"')";
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+    bot.sendMessage(userId, 'Отлично! Я обязательно напомню, если не сдохну :)');
+});
+
+
+setInterval(() => {
+    time = formattedTime();
+    // console.log(time);
+    // var users_id = connection.query('SELECT user_id FROM reminders WHERE created_at = formattedTime', function (error, results, fields) {console.log(error)});
+    // var telegram_users_id = connection.query('SELECT telegram_id FROM users', function (error, results, fields) {console.log(error)});
+    // console.log("AAAAAAAAAA" . users_id);
+    // console.log("BBBBBBBBBB" . telegram_users_id);
+    // // connection.release();
+
+    // for (var i=0; i <= users_id; i++) {
+    //     if (telegram_users_id[i] == users_id[i]) {
+    //         bot.sendMessage(query[i], 'Received your message');
+    // }
+}, 1000);
