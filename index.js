4dd245f35696deb5dbdd26b44880f23806a42662
IGorRozhkov <zawert23879@gmail.com>
2017-11-11 03:18:18 +0300

    commit 2

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 const TelegramBot = require('node-telegram-bot-api');
+
+const mysql = require('mysql');
+
+const cMysql = mysql.createPool({
+        database : 'test',
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        connectionLimit: 100
+    });
+
+cMysql.getConnection(function(err,conn){
+    if(err){
+        console.log("MYSQL: can't get connection from pool:",err)
+    }else {
+        conn.query("SELECT * FROM test",
+            function(er,rows){
+                if(er){
+                    conn.release();
+                    console.log("MYSQL: ERROR: ",err);
+                } else {
+                    conn.release();
+                    console.log(rows);
+                    // make someting with rows
+                }
+            });
+    }
+});
+
+
+
  
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '452480076:AAE4bSOiAeZO2iDmcdgnPfROZxVNUUEcu20';
@@ -7,6 +38,7 @@ const token = '452480076:AAE4bSOiAeZO2iDmcdgnPfROZxVNUUEcu20';
 const bot = new TelegramBot(token, {polling: true});
  
 // Matches "/echo [whatever]"
+
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
@@ -21,9 +53,45 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
  
 // Listen for any kind of message. There are different kinds of
 // messages.
+
 bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
- 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+    const chatId = msg.chat.id;
+messageLog();
+addinsql(msg.text);
+// send a message to the chat acknowledging receipt of their message
+bot.sendMessage(chatId, 'Received your message');
 });
+
+
+
+
+
+
+function messageLog(msg){
+// console.log(msg);
+// console.log(msg.from.username+':'+msg.text);
+};
+
+function addinsql(name) {
+    cMysql.getConnection(function(err,conn){
+        if(err){
+            console.log("MYSQL: can't get connection from pool:",err)
+        }else {
+            conn.query("INSERT INTO test (name) VALUES ('"+name+"')",
+                function(er,rows){
+                    if(er){
+                        conn.release();
+                        console.log("MYSQL: ERROR: ",err);
+                    } else {
+
+                        conn.release();
+
+                        // make someting with rows
+                    }
+                });
+        }
+    });
+
+
+}
+
